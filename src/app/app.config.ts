d74321ef81fede152ee20dc8585f73a2a0236ab3
BlueInt32 <simon.budin@gmail.com>
2024-10-24 04:22:46 +0200

    Add highlightjs libs

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,8 +2,18 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideHighlightOptions } from 'ngx-highlightjs';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideHttpClient(withFetch()), provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideHttpClient(withFetch()),
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideHighlightOptions({
+      fullLibraryLoader: () => import('highlight.js'),
+      lineNumbersLoader: () => import('ngx-highlightjs/line-numbers'),
+      themePath: 'github-dark.scss',
+    }),
+  ],
 };
--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -2,19 +2,29 @@ import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
-
+import { Highlight } from 'ngx-highlightjs';
+import { HighlightLineNumbers } from 'ngx-highlightjs/line-numbers';
+import { esServerContainerCreationScript } from '../code-samples/dockers/es';
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    RouterLink,
+    RouterLinkActive,
+    Highlight,
+    HighlightLineNumbers,
+  ],
   templateUrl: './home-page.component.html',
-  styleUrl: './home-page.component.scss'
+  styleUrl: './home-page.component.scss',
 })
 export class HomePageComponent {
   constructor(private http: HttpClient) {}
-  baseUrl= 'http://localhost:9200';
+  baseUrl = 'http://localhost:9200';
   elasticServerVersion = '';
   errorText = '';
+  esServerContainerCreationScript = esServerContainerCreationScript;
 
   esServerConnectionClickHandler() {
     this.http.get(`${this.baseUrl}`).subscribe({
